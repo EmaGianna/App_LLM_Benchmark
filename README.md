70ae200abfb9f21e4ea1a0b7cdd95353d8e2f05e
Emanuel Giannattasio <emanuel.giannattasio@gmail.com>
2024-07-29 20:41:05 -0300

    update_20240729
    Se ordena la pagina de graficas

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # App_LLM_Benchmark
 Streamlit App That Show LLM_Benchmark
+The Details of analysis you could find [here](https://github.com/EmaGianna/LLM_Benchmark)
--- llm_benchmark_app/lib_llm_benchmark/utils.py
+++ llm_benchmark_app/lib_llm_benchmark/utils.py
@@ -1,5 +1,7 @@
 import plotly.graph_objects as go
 import streamlit as st
+import pandas as pd 
+
 
 def list_to_dict(in_list):
     """
@@ -53,13 +55,25 @@ def create_scatter_graph(df_llama, df_gemma, column):
 
     # Crear la figura
     fig = go.Figure()
-
     # Añadir la primera gráfica de líneas (Llama)
-    fig.add_trace(go.Scatter(x=df_llama['id_promtp'], y=df_llama[column],
-                             mode='lines+markers', name='Llama'))
-    ## Añadir la segunda gráfica de líneas (Gemma)
-    fig.add_trace(go.Scatter(x=df_gemma['id_promtp'], y=df_gemma[column],
-                             mode='lines+markers', name='Gemma'))
+    fig.add_trace(go.Scatter(x=df_llama['id_promtp'], 
+                             y=df_llama[column],
+                             mode='lines+markers', 
+                             name='Llama',
+                             #line=dict(color='blue'),
+                             #marker=dict(color='blue')
+                             )
+                    )
+
+    # Añadir la segunda gráfica de líneas (Gemma)
+    fig.add_trace(go.Scatter(x=df_gemma['id_promtp'], 
+                             y=df_gemma[column],
+                             mode='lines+markers',
+                             name='Gemma',
+                             #line=dict(color='red'),
+                             #marker=dict(color='red')
+                             )
+                  )
 
     # Configurar el título y los ejes
     fig.update_layout(
@@ -75,3 +89,91 @@ def create_scatter_graph(df_llama, df_gemma, column):
     )
     # Mostrar el gráfico
     st.plotly_chart(fig)
+
+def print_all_scatter_graph(df_num_llama, df_num_gemma, df_llm_llama, df_llm_gemma):
+    """
+    The function `print_all_scatter_graph` creates scatter graphs for numerical columns in two
+    DataFrames, compares the values, determines the better indicator, and displays the results.
+    
+    :param df_num_llama: It looks like the code snippet you provided is a function that generates
+    scatter graphs and compares values between two DataFrames. However, the definition of `df_num_llama`
+    is missing in the code snippet you shared. Could you please provide the definition of `df_num_llama`
+    so that I can
+    :param df_num_gemma: It looks like the code snippet you provided is a function that generates
+    scatter graphs and compares values between two DataFrames. However, the definition of `df_num_gemma`
+    is missing in the code snippet you shared
+    """
+
+    cols = df_num_gemma.columns.drop('id_promtp')
+    columnas = st.multiselect('Elija Área/País: ', cols, cols[0])
+    
+    for col in columnas:
+        #if col != 'id_promtp':
+        create_scatter_graph(df_num_llama, df_num_gemma, col)
+
+        # Concatenar los DataFrames con sufijos
+        df_compare = pd.concat([
+            df_llm_gemma[['modelo llm', col]].add_suffix("_gemma"),
+            df_llm_llama[['modelo llm', col]].add_suffix("_llama")
+        ], axis=1)
+
+        # Comparar los valores y determinar el mejor indicador
+        mejor_indicador = df_compare[[f'{col}_gemma', f'{col}_llama']].apply(
+            lambda row: 'gemma' if row[f'{col}_gemma'] < row[f'{col}_llama'] else (
+                'llama2' if row[f'{col}_gemma'] > row[f'{col}_llama'] else 'iguales'
+            ), axis=1
+        )
+
+        # Añadir la columna resultante al DataFrame original
+        df_compare['mejor_llm'] = mejor_indicador
+        # Agrupar y contar por el campo mejor_llm
+        resultado = df_compare.groupby('mejor_llm').size().reset_index(name='cantidad')
+        # Mostrar el resultado
+        st.markdown(f"""**La grafica precedente se realizo con el siguiente set de datos:**""")
+        st.dataframe(resultado)
+
+
+def print_rpt_comparison(df_llm_gemma, df_llm_llama):
+    """
+    The function `print_rpt_comparision` compares the average values of numeric columns between two
+    dataframes and generates a report highlighting the differences.
+    
+    :param df_llm_gemma: The function `print_rpt_comparision` compares the average values of numeric
+    columns between two DataFrames `df_llm_gemma` and `df_llm_llama`. It calculates the average values
+    for each numeric column in both DataFrames and then compares them to determine which DataFrame has
+    better average
+    :param df_llm_llama: The function `print_rpt_comparision` compares the average values of different
+    numeric columns between two dataframes `df_llm_gemma` and `df_llm_llama`. It calculates the average
+    values for each numeric column in both dataframes and then compares them to determine which
+    dataframe has better
+    """
+    # Comparacion de los valores de cada indicador obtenido para llama2 y gemma
+    numeric_cols = ['tiempo de lectura','cantidad de oraciones','cantidad de caracteres','cantidad de letras'
+                ,'cantidad de silabas','largo promedio de oraciones','promedio de letras por palabra','cantidad de stopwords'
+                ,'cantidad de palabras','densidad lexica','riqueza lexica']
+
+    #Promedio los valores de cada variables/columnas numericas de gemma y las guardo en un diccionario
+    dict_prom_gemma = df_llm_gemma[numeric_cols].mean().to_dict()
+    #Promedio los valores de cada variables/columnas numericas de llama2 y las guardo en un diccionario
+    dict_prom_llama2 = df_llm_llama[numeric_cols].mean().to_dict()
+
+    keys = dict_prom_gemma.keys()
+    st.markdown('## **Reporte de Comparación**\n')
+
+    for key in keys:
+        st.markdown(f'### **Comparacion del promedio de {key}**')
+        if dict_prom_gemma[key] == dict_prom_llama2[key]:
+            st.markdown( f"""El valor promedio de los valores obtenidos para {key} son identicos. 
+                             No se puede determinar cual es el mejor en este aspecto\n
+                            gemma: {round(dict_prom_gemma[key], 2)} - llama2: {round(dict_prom_llama2[key], 2)}
+                            """
+                        )
+        elif dict_prom_gemma[key] < dict_prom_llama2[key]:
+            st.markdown( f"""El valor promedio de los valores obtenidos para {key} son mejores en gemma:7b por sobre llama2\n.
+                             gemma: {round(dict_prom_gemma[key], 2)} - llama2: {round(dict_prom_llama2[key], 2)}\n"""
+                        )
+        elif dict_prom_gemma[key] > dict_prom_llama2[key]:
+            st.markdown( f"""El valor promedio de los valores obtenidos para {key} son mejores en llama2 por sobre gemma\n.
+                           gemma: {round(dict_prom_gemma[key], 2)} - llama2: {round(dict_prom_llama2[key], 2)}\n""")
+        else:
+            st.text( f'Para el indicador promedio {key} no se puede determinar benchmark\n')
