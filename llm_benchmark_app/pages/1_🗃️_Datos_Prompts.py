456cbbd56a9bae666511b60aaffd503adfe3b3a9
Emanuel Giannattasio <emanuel.giannattasio@gmail.com>
2024-07-29 21:45:35 -0300

    update20240729-final
    update20240729-final

--- llm_benchmark_app/pages/1_🗃️_Datos_Prompts.py
+++ llm_benchmark_app/pages/1_🗃️_Datos_Prompts.py
@@ -2,7 +2,7 @@ import streamlit as st
 import datetime
 import sqlite3
 import pandas as pd 
-#from loguru import logger
+from loguru import logger
 
 from lib_llm_benchmark import staticals_functions
 # from lib_llm_benchmark import utils
@@ -80,4 +80,4 @@ if __name__ == '__main__':
     st.session_state["key"] = llm_df      
 ###################################################
     end_time = datetime.datetime.now()
-    #logger.debug(f'tiempo de ejecucion:{str(end_time - begin_time)}')
+    logger.debug(f'tiempo de ejecucion:{str(end_time - begin_time)}')
